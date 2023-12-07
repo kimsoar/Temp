55e279a289c73e0aecf006ffb2bd9387884aa04b
kimsoar <kimsoar17@gmail.com>
2023-12-07 22:08:12 +0900

    add checkbox

--- toggle/sample/src/components/HelloWorld.vue
+++ toggle/sample/src/components/HelloWorld.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="container">
-    <SkewedToggle offText="OFF" onText="ON" class="toggle" v-model="skewedToggle"/>
     <BasicInput v-model="email"/>
     <button @click="testClick">click</button>
+    <div style="display: flex; align-items: center;">
+      <label class="label-checkbox">
+        <input type="checkbox">
+        <span>테스트1</span>
+    </label>
+      <SkewedToggle offText="AND" onText="OR" class="toggle" v-model="skewedToggle"/>
+      <label class="label-checkbox">
+        <input type="checkbox">
+        <span>Checkbox</span>
+    </label>
+=
+      <button>조회</button>
+    </div>
   </div>
 </template>
 
@@ -38,6 +50,36 @@ export default {
     width: 50px;
     height: 30px;
   } */
+
+  label.label-checkbox {
+  cursor: pointer;
+}
+
+
+label.label-checkbox input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  visibility: hidden;
+  pointer-events: none;
+}
+
+label.label-checkbox span {
+  /* padding: 11px 21px; */
+  border: 1px solid #ccc;
+  display: inline-block;
+  color: #202020;
+  border-radius: 6px;
+  margin: 7px;
+  background: #f5f5f5;
+  user-select: none;
+}
+
+label.label-checkbox input:checked + span {
+  box-shadow: inset 1px 2px 5px #777;
+  transform: translateY(1px);
+  background: #e5e5e5;
+}
 }
 
 </style>
